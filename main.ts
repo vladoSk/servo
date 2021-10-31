98a3c3d387debc49b1ae2734339bf807307d8df5
vladoSk <31918047+vladoSk@users.noreply.github.com>
2021-10-31 07:29:18 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
